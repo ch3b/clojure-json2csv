d39d0739fb306882dde939398af8c881371b43af
ch3b <c.s.huttonbrown@gmail.com>
2023-09-03 16:36:46 +0100

    Project setup

---
+++ test/clojure_json2csv/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-json2csv.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-json2csv.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
